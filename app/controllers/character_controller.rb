f0e0f624a0c315a0c37b65624dc785a0f7c3e091
Manik Hosen <rahathossenmanik@gmail.com>
2023-09-05 00:35:48 +0600

    Danger

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -2,4 +2,37 @@ class CharacterController < ApplicationController
   def findAll
     render json: {first_name: 'Sam', last_name: 'Thomas', genre: 'rock'}, status: :ok
   end
+
+  def index
+    @characters = Character.all
+    puts @characters
+    render json: @characters, status: :ok
+  end
+
+  def show
+    @character = Character.find(params[:id])
+    fresh_when etag: @character
+  end
+
+  def create
+    character = Character.create!(character_params)
+    redirect_to character
+  end
+
+  def update
+    character = Character.find(params[:id])
+    character.update!(character_params)
+    redirect_to character
+  end
+
+  def destroy
+    character = Character.find(params[:id])
+    character.destroy!
+    redirect_to characters_path
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get "/character/getall", to: "character#findAll"
+  get "/characters/getall", to: "character#index"
+  get "/characters/getbyid/:id", to: "character#show"
+  post "/characters/create", to: "character#create"
+  put "/characters/update/:id", to: "character#update"
+  delete "/characters/delete/:id", to: "character#destroy"
 end
