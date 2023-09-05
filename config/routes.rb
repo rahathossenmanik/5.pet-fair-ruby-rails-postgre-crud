273d7cebd4f25f2a074196cf94751e2012694cc5
Manik Hosen <manik.hosen@bizzntek.com>
2023-09-05 15:34:21 +0600

    basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,25 @@ Rails.application.routes.draw do
  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  # Character Routes
   get "/characters/getall", to: "character#index"
   get "/characters/getbyid/:id", to: "character#show"
   post "/characters/create", to: "character#create"
   put "/characters/update/:id", to: "character#update"
   delete "/characters/delete/:id", to: "character#destroy"
+
+  # Pet Type Routes
+  get "/pettypes/getall", to: "pet_type#index"
+  get "/pettypes/getbyid/:id", to: "pet_type#show"
+  post "/pettypes/create", to: "pet_type#create"
+  put "/pettypes/update/:id", to: "pet_type#update"
+  delete "/pettypes/delete/:id", to: "pet_type#destroy"
+
+  # Pet Routes
+  get "/pets/getall", to: "pet#index"
+  get "/pets/getbyid/:id", to: "pet#show"
+  post "/pets/create", to: "pet#create"
+  put "/pets/update/:id", to: "pet#update"
+  delete "/pets/delete/:id", to: "pet#destroy"
 end
